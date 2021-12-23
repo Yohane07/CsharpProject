562e68202463e4995f961c89c4c6572f4fca14a6
pageAccueil <yohaneendoke4@gmail.com>
2021-12-23 22:53:11 +0100

    first commit to get new branch

---
+++ IPersonneServices.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RembourseMoi
+{
+    interface IPersonneServices
+    {
+        public List<Personne> GetAllPersonnes();
+        public Personne GetPersonneById(int id);
+        public Personne Insert(Personne p);
+        public Personne Update(Personne p);
+        public Personne CalculeDepenses(int depense); //Du coup c'est ici que se passe le plus gros du bouleau, implémenter le calcul quoiqu'avec le getAllPersonne je pourrai avoir le nb de personne et calculer la mopyenne
+        public void Delete(Personne p);
+    }
+}
---
+++ Personne.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RembourseMoi
+{
+    class Personne
+    {
+        public int Id;
+        public string Nom;
+        public string Prenom;
+        public int Depense;
+
+        public Personne(string nom, string prenom, int depense)
+        {
+            Nom = nom;
+            Prenom = prenom;
+            Depense = depense;
+        }
+
+    }
+}
+
